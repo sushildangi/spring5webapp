a9e6249fd55894a947d9ad699ff78aa60851224e
sushildangi <dangi.sushil5@gmail.com>
2018-10-17 17:00:07 +0530

    Thymeleaf added and BookController added

---
+++ src/main/java/org/pyarts/spring5webapp/controller/BookController.java
@@ -0,0 +1,24 @@
+package org.pyarts.spring5webapp.controller;
+
+import org.pyarts.spring5webapp.repositories.BookRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class BookController {
+
+    private BookRepository bookRepository;
+
+    public BookController(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    @GetMapping("/books")
+    public String getBooks(Model model) {
+
+        model.addAttribute("books",bookRepository.findAll());
+
+        return "books";
+    }
+}
